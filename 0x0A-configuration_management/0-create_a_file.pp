ad522a99ee1cc7e6cccf7743e68054ad395e9294
AndZapata <andzapata514@gmail.com>
2019-08-05 22:05:55 +0000

    [Feature] Create a file in tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# coment: The current task creates a File in the /tmp path
+file { '/tmp/holberton':
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love puppet',
+}
