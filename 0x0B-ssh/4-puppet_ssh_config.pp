0d17bef962e7f13d8ccc57dbefd471ef04dccef5
AndZapata <andzapata514@gmail.com>
2019-08-06 15:56:58 +0000

    [Feature] first advanced task

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,13 @@
 # Comment: Current task modify file-config ssh
 file { '/etc/ssh/ssh_config':
   ensure => present,
-  target => '/etc/ssh/ssh_config',
-}
+}->
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^PasswordAuthentication',
-}
+}->
 file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
