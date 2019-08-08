38cd90e924f61282b1ee5aabb8cbeaa2ebc17407
AndZapata <andzapata514@gmail.com>
2019-08-08 14:27:25 +0000

    [Feature] execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Coment: The current task terminate the script killmenow
 exec { 'killmenow':
   command => 'pkill killmenow',
-  path    => '/usr/bin/',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
