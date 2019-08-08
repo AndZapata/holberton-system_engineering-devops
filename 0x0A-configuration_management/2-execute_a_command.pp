d47509f7a7a98e446263f3ce13fdb7ca64133ab1
AndZapata <andzapata514@gmail.com>
2019-08-08 20:09:40 +0000

    [Fix] -f

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Coment: The current task terminate the script killmenow
 exec { 'killmenow':
-  command => pkill -f 'killmenow',
+  command => 'pkill -f killmenow',
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
