caf59f90bb6fc2dfce0c72aac47b1735a98540fa
AndZapata <andzapata514@gmail.com>
2019-08-17 00:42:06 +0000

    [Feature] Advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,8 @@
 # Comment: Current task puppet install, file and rewrite 301 and restart server
-package { 'nginx':
+exec { 'update server':
+  command => '/usr/bin/apt-get update',
+}
+-> package { 'nginx':
   ensure => present,
 }
 -> file { '/etc/nginx/sites-available/default':
@@ -7,9 +10,10 @@ package { 'nginx':
 }
 -> file_line { 'rewrite a line':
   ensure => present,
-  path => '/etc/nginx/sites-available/default',
-  line => 'add_header X-Served-By ${hostname}',
-  match => '^location / {',
+  path   => '/etc/nginx/sites-available/default',
+  line   => "   location / {
+        add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
 }
 -> exec { 'initialize server':
   command => '/usr/sbin/service nginx restart',
