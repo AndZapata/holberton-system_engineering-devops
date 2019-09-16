ea3fdb91dbf107e8696bb0dc8a6267235a6ad8c4
AndZapata <andzapata514@gmail.com>
2019-09-16 23:08:21 +0000

    [Fix] output

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,21 +5,13 @@ if __name__ == "__main__":
     from requests import get, post
     from sys import argv
     users = get('https://jsonplaceholder.typicode.com/users').json()
-    list1 = []
-    dict_ob2 = {}
-    val = []
-    for i in users:
-        list1.append(i.get('id'))
-        for usr_id in list1:
-            l_todo = get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                         .format(usr_id)).json()
-            for j in l_todo:
-                dict1 = {}
-                dict1.update({'task': j.get('title')})
-                dict1.update({'completed': j.get('completed')})
-                dict1.update({'username': i.get('username')})
-                val.append(dict1)
-    dict_ob2.update({usr_id: val})
-    with open('todo_all_employees.json'.format(usr_id),
-              'w', newline='') as json_file:
-        json_file.write(json.dumps(dict_ob2) + '\n')
+    l_todo = get('https://jsonplaceholder.typicode.com/todos').json()
+    dict2 = {i.get('id'): [{'username': i.get('username')}] for i in users}
+    for j in l_todo:
+        dict1 = {}
+        usr_id = j.get('userId')
+        dict1.update({'task': j.get('title')})
+        dict1.update({'completed': j.get('completed')})
+        dict2.get(usr_id).append(dict1)
+    with open('todo_all_employees.json', 'w') as json_file:
+            json.dump(dict2, json_file)
